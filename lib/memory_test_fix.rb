fcf37570c676adebc0dab6dec86ecd4fbdeba0d4
topfunky <topfunky@798a5b91-ee08-0410-b43b-a734d69ea54e>
2006-06-01 21:12:08 +0000

    Undid external rake task
    git-svn-id: http://topfunky.net/svn/plugins/memory_test_fix@89 798a5b91-ee08-0410-b43b-a734d69ea54e

--- lib/memory_test_fix.rb
+++ lib/memory_test_fix.rb
@@ -6,8 +6,6 @@ def in_memory_database?
 end
 
 if in_memory_database?
-  puts "Dumping Schema"
-  system "rake db_schema_dump" # or db:schema:dump for Rails 1.1
   puts "Creating sqlite in memory database"
   load "#{RAILS_ROOT}/db/schema.rb" # use db agnostic schema by default
 #  ActiveRecord::Migrator.up('db/migrate') # use migrations

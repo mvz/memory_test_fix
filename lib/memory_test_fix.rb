c1a08c082f91a08df3665963fb5ce007ca71d8eb
Erik Hanson & Matt Scilipoti <pair+erik+matt@pivotallabs.com>
2010-03-29 01:36:06 -0400

    Support environments besides test (cucumber, etc).

--- lib/memory_test_fix.rb
+++ lib/memory_test_fix.rb
@@ -1,10 +1,8 @@
-
 # Update: Looks for the SQLite and SQLite3 adapters for
 # compatibility with Rails 1.2.2 and also older versions.
 def in_memory_database?
-  if ENV["RAILS_ENV"] == "test" and
-    (Rails::Configuration.new.database_configuration['test']['database'] == ':memory:' or
-    Rails::Configuration.new.database_configuration['test']['dbfile'] == ':memory:')
+  if (Rails::Configuration.new.database_configuration[Rails.env]['database'] == ':memory:' or
+    Rails::Configuration.new.database_configuration[Rails.env]['dbfile'] == ':memory:')
     begin
       if ActiveRecord::Base.connection.class == ActiveRecord::ConnectionAdapters::SQLite3Adapter
         return true
@@ -14,8 +12,9 @@ def in_memory_database?
         return true
       end
     end
+
+    false
   end
-  false
 end
 
 def verbosity
@@ -29,16 +28,16 @@ end
 if in_memory_database?
   load_schema = lambda {
     load "#{RAILS_ROOT}/db/schema.rb" # use db agnostic schema by default
-    #  ActiveRecord::Migrator.up('db/migrate') # use migrations
+  #  ActiveRecord::Migrator.up('db/migrate') # use migrations
   }
   case verbosity
-  when "silent"
-    silence_stream(STDOUT, &load_schema)
-  when "quiet"
-    inform_using_in_memory
-    silence_stream(STDOUT, &load_schema)
-  else
-    inform_using_in_memory
-    load_schema.call
+    when "silent"
+      silence_stream(STDOUT, &load_schema)
+    when "quiet"
+      inform_using_in_memory
+      silence_stream(STDOUT, &load_schema)
+    else
+      inform_using_in_memory
+      load_schema.call
   end
 end

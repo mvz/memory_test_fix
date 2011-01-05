0ae496b66b0a720689ec555f576eb41dcb831ca3
Matijs van Zuijlen <matijs@matijs.net>
2011-01-06 00:25:32 +0100

    Use Rails 3 gem plugin loading mechanism, with initialization hook.
    Without this change, the code runs too soon, when the configuration has not
    been initialized yet.

--- lib/memory_test_fix.rb
+++ lib/memory_test_fix.rb
@@ -1,34 +1,42 @@
-def in_memory_database?
-  dc = Rails.configuration.database_configuration[Rails.env]
-  if (dc['database'] == ':memory:' or dc['dbfile'] == ':memory:')
-    if ActiveRecord::Base.connection.class == ActiveRecord::ConnectionAdapters::SQLite3Adapter
-      return true
+module MemoryTestFix
+  def self.in_memory_database?
+    dc = Rails.configuration.database_configuration[Rails.env]
+    if (dc['database'] == ':memory:' or dc['dbfile'] == ':memory:')
+      if ActiveRecord::Base.connection.class == ActiveRecord::ConnectionAdapters::SQLite3Adapter
+	return true
+      end
+    end
+    false
+  end
+
+  def self.verbosity
+    Rails::Configuration.new.database_configuration[Rails.env]['verbosity']
+  end
+
+  def self.inform_using_in_memory
+    puts "Creating sqlite :memory: database"
+  end
+
+  def self.init_schema
+    if in_memory_database?
+      load_schema = lambda {
+	load "#{Rails.root}/db/schema.rb" # use db agnostic schema by default
+	#  ActiveRecord::Migrator.up('db/migrate') # use migrations
+      }
+      case verbosity
+      when "silent"
+	silence_stream(STDOUT, &load_schema)
+      when "quiet"
+	inform_using_in_memory
+	silence_stream(STDOUT, &load_schema)
+      else
+	inform_using_in_memory
+	load_schema.call
+      end
     end
   end
-  false
 end
 
-def verbosity
-  Rails::Configuration.new.database_configuration[Rails.env]['verbosity']
-end
-
-def inform_using_in_memory
-  puts "Creating sqlite :memory: database"
-end
-
-if in_memory_database?
-  load_schema = lambda {
-    load "#{Rails.root}/db/schema.rb" # use db agnostic schema by default
-    #  ActiveRecord::Migrator.up('db/migrate') # use migrations
-  }
-  case verbosity
-  when "silent"
-    silence_stream(STDOUT, &load_schema)
-  when "quiet"
-    inform_using_in_memory
-    silence_stream(STDOUT, &load_schema)
-  else
-    inform_using_in_memory
-    load_schema.call
-  end
+ActiveSupport.on_load(:before_initialize) do
+  MemoryTestFix.init_schema
 end

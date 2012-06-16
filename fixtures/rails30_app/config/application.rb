9b848bf7953bef5bbb45d35e5967524ee9e59863
Matijs van Zuijlen <matijs@matijs.net>
2012-06-16 09:26:53 +0200

    Create bare-minimum Rails 3.0 app for testing

---
+++ fixtures/rails30_app/config/application.rb
@@ -0,0 +1,12 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+Bundler.require(:default, Rails.env) if defined?(Bundler)
+
+module Rails30App
+  class Application < Rails::Application
+    config.encoding = "utf-8"
+    config.filter_parameters += [:password, :ssn]
+  end
+end

3bdad3409c47285cd2207edc42b2c719530e7388
Matijs van Zuijlen <matijs@matijs.net>
2013-01-30 07:48:43 +0100

    Return to the current directory after each integration test

--- test/integration/integration_test.rb
+++ test/integration/integration_test.rb
@@ -3,16 +3,18 @@ require 'minitest/spec'
 
 describe "A Rails 3.0 app using memory_test_fix" do
   it "can run its tests without a real db" do
-    Dir.chdir 'fixtures/rails30_app'
-    system(*%w(bundle update))
-    system(*%w(bundle exec rake))
+    Dir.chdir 'fixtures/rails30_app' do
+      system(*%w(bundle update))
+      system(*%w(bundle exec rake))
+    end
   end
 end
 
 describe "A Rails 3.2 app using memory_test_fix" do
   it "can run its tests without a real db" do
-    Dir.chdir 'fixtures/rails32_app'
-    system(*%w(bundle update))
-    system(*%w(bundle exec rake))
+    Dir.chdir 'fixtures/rails32_app' do
+      system(*%w(bundle update))
+      system(*%w(bundle exec rake))
+    end
   end
 end

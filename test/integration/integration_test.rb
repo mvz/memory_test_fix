5e829d6a3e63d290ee1627e028eda314e6be859c
Matijs van Zuijlen <matijs@matijs.net>
2013-01-30 07:45:33 +0100

    Add integration test

---
+++ test/integration/integration_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/spec'
+
+describe "A Rails 3.0 app using memory_test_fix" do
+  it "can run its tests without a real db" do
+    Dir.chdir 'fixtures/rails30_app'
+    system(*%w(bundle update))
+    system(*%w(bundle exec rake))
+  end
+end

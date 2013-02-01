57c963f0a9f06446ad2beaed86157b70c660272d
Matijs van Zuijlen <matijs@matijs.net>
2013-02-01 23:21:27 +0100

    Make sure integration tests failures are reported as such

--- test/integration/integration_test.rb
+++ test/integration/integration_test.rb
@@ -4,8 +4,8 @@ require 'minitest/spec'
 describe "A Rails 3.0 app using memory_test_fix" do
   it "can run its tests without a real db" do
     Dir.chdir 'fixtures/rails30_app' do
-      system(*%w(bundle update))
-      system(*%w(bundle exec rake))
+      system(*%w(bundle update)).must_equal true
+      system(*%w(bundle exec rake)).must_equal true
     end
   end
 end
@@ -13,8 +13,8 @@ end
 describe "A Rails 3.2 app using memory_test_fix" do
   it "can run its tests without a real db" do
     Dir.chdir 'fixtures/rails32_app' do
-      system(*%w(bundle update))
-      system(*%w(bundle exec rake))
+      system(*%w(bundle update)).must_equal true
+      system(*%w(bundle exec rake)).must_equal true
     end
   end
 end

e633a3c8d684245e3882221097370de012a9b6e6
Matijs van Zuijlen <matijs@matijs.net>
2013-02-02 09:08:58 +0100

    Test output from fixture test runs
    This makes sure the memory test fix is actually activated.

--- test/integration/integration_test.rb
+++ test/integration/integration_test.rb
@@ -6,11 +6,20 @@ def update_bundle
     raise "Unable to initialize test environment"
 end
 
+def run_tests
+  out, _ = capture_subprocess_io do
+    system(*%w(bundle exec rake)).must_equal true
+  end
+  out.must_match(/Creating sqlite :memory: database/)
+  out.must_match(/initialize_schema_migrations_table/)
+  out.must_match(/assume_migrated_upto_version/)
+end
+
 describe "A Rails 3.0 app using memory_test_fix" do
   it "can run its tests without a real db" do
     Dir.chdir 'fixtures/rails30_app' do
       update_bundle
-      system(*%w(bundle exec rake)).must_equal true
+      run_tests
     end
   end
 end
@@ -19,7 +28,7 @@ describe "A Rails 3.2 app using memory_test_fix" do
   it "can run its tests without a real db" do
     Dir.chdir 'fixtures/rails32_app' do
       update_bundle
-      system(*%w(bundle exec rake)).must_equal true
+      run_tests
     end
   end
 end

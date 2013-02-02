3c23f711fbfd051775e6339fc1e0138c9ea8c39c
Matijs van Zuijlen <matijs@matijs.net>
2013-02-02 08:57:39 +0100

    Stop Rails 3 complaining about where to put its deprecation messages

---
+++ fixtures/rails30_app/config/environments/test.rb
@@ -0,0 +1,3 @@
+Rails30App::Application.configure do
+  config.active_support.deprecation = :stderr
+end
---
+++ fixtures/rails32_app/config/environments/test.rb
@@ -0,0 +1,3 @@
+Rails32App::Application.configure do
+  config.active_support.deprecation = :stderr
+end

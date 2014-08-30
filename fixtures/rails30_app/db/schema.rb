313442454ddf5e62657fa70a78432083136bbdf4
Matijs van Zuijlen <matijs@matijs.net>
2014-08-30 09:24:56 +0200

    Add an actual model to the fixtures

--- fixtures/rails30_app/db/schema.rb
+++ fixtures/rails30_app/db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20140830065127) do
+
+  create_table "foos", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end

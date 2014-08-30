1d61a2078f5dc54ee0b0630d5a3256d46e9b2470
Matijs van Zuijlen <matijs@matijs.net>
2014-08-30 12:11:21 +0200

    Add hack to support rails 4.0

--- lib/memory_test_fix.rb
+++ lib/memory_test_fix.rb
@@ -1,2 +1,15 @@
 require 'memory_test_fix/schema_loader'
-require 'memory_test_fix/railtie.rb' if defined?(Rails)
+
+if defined?(Rails)
+  require 'memory_test_fix/railtie.rb'
+
+  if Rails.version =~ /^4\.0\./
+    ActiveRecord::Base.class_eval do
+      def self.establish_connection(*_)
+        super.tap do
+          MemoryTestFix::SchemaLoader.init_schema
+        end
+      end
+    end
+  end
+end

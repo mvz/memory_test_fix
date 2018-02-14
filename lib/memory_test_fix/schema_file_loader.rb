7903986b9a4da851d745959454185a06bb6b6c06
Matijs van Zuijlen <matijs@matijs.net>
2018-02-14 14:42:02 +0100

    Remove obsolete TODO

--- lib/memory_test_fix/schema_file_loader.rb
+++ lib/memory_test_fix/schema_file_loader.rb
@@ -4,7 +4,6 @@ module MemoryTestFix
   module SchemaFileLoader
     # Load the Rails schema file.
     def self.load_schema
-      # TODO: Use tooling provided by rails once support for Rails 3 is dropped.
       load "#{Rails.root}/db/schema.rb"
     end
   end
